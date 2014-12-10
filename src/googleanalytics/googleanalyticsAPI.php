4d2e1c4e4d552d0b4cb8210e455a2cb503aaceea
Paulo Veneroso <venerosobrasil@gmail.com>
2014-12-10 20:42:02 -0200

    api teste AB

--- src/googleanalytics/googleanalyticsAPI.php
+++ src/googleanalytics/googleanalyticsAPI.php
@@ -27,4 +27,28 @@ class googleanalyticsAPI extends \classes\Interfaces\resource{
         else{return $str;}
     }
     
+    
+    public function abTest($keys){
+        if(strstr($_SERVER['HTTP_HOST'], ".") !== false){
+        if(!is_array($keys))return;
+        foreach($keys as $page => $key){
+            if(CURRENT_MODULE.'/'.CURRENT_CONTROLLER.'/'.CURRENT_ACTION != $page)continue;  
+            echo "<!-- Google Analytics Content Experiment code -->
+                    <script>function utmx_section(){}function utmx(){}(function(){var
+                    k='$key',d=document,l=d.location,c=d.cookie;
+                    if(l.search.indexOf('utm_expid='+k)>0)return;
+                    function f(n){if(c){var i=c.indexOf(n+'=');if(i>-1){var j=c.
+                    indexOf(';',i);return escape(c.substring(i+n.length+1,j<0?c.
+                    length:j))}}}var x=f('__utmx'),xx=f('__utmxx'),h=l.hash;d.write(
+                    '<sc'+'ript src=\"'+'http'+(l.protocol=='https:'?'s://ssl':
+                    '://www')+'.google-analytics.com/ga_exp.js?'+'utmxkey='+k+
+                    '&utmx='+(x?x:'')+'&utmxx='+(xx?xx:'')+'&utmxtime='+new Date().
+                    valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+
+                    '\" type=\"text/javascript\" charset=\"utf-8\"><\/sc'+'ript>')})();
+                    </script><script>utmx('url','A/B');</script>
+                    <!-- End of Google Analytics Content Experiment code -->";
+        }
+       }
+    }
+    
 }
